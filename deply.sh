bc4968921e839b96845747a2946534e68a94e525
coderhzy <1257664828@qq.com>
2022-05-04 22:37:47 +0800

    feat(修改部署): ✨ 修改部署

--- deply.sh
+++ deply.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env sh
 
+
+# 推送源码
+git push -f git@github.com:codehzy/vitepress-starter.git master
+
 # 确保脚本抛出遇到的错误
 set -e
 
 # 生成静态文件
 npm run docs:build
 
+
 # 进入生成的文件夹
-cd docs/.vuepress/dist
+cd docs/.vitepress/dist
 
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
@@ -18,9 +23,6 @@ git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
-
-# 推送源码
-git push -f git@github.com:codehzy/vitepress-starter.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:codehzy/vitepress-starter.git master:gh-pages
